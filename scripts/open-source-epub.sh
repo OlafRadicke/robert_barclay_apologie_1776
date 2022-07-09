8479fce70a3a748a5d0639bb9f1b84890bf440ce
Olaf Radicke <briefkasten@olaf-radicke.de>
2022-07-09 17:32:50 +0200

    WIP

--- scripts/open-source-epub.sh
+++ scripts/open-source-epub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sigil ../../fremd_texte/brclay_robert/robert-barclay-s-apologie-1776-google-scan.epub &
+# sigil ../../fremd_texte/brclay_robert/robert-barclay-s-apologie-1776-google-scan.epub &
 okular ../../fremd_texte/brclay_robert/robert-barclay-s-apologie-1776-google-scan.pdf &
