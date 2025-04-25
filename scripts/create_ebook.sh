e9f28b214f6ded5705a9e129e929b5f19cae28d9
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-04-25 16:50:08 +0200

    Update.

--- scripts/create_ebook.sh
+++ scripts/create_ebook.sh
@@ -4,23 +4,23 @@ set -x
 mkdir -p ./var
 rm ./var/apologie_r_barclay.epub ./var/joined_code.md
 
-cat markdown_source/00/*.md > ./var/joined_code.md
-cat markdown_source/01/*.md >> ./var/joined_code.md
-cat markdown_source/02/*.md >> ./var/joined_code.md
-cat markdown_source/03/*.md >> ./var/joined_code.md
-cat markdown_source/04/*.md >> ./var/joined_code.md
-cat markdown_source/05/*.md >> ./var/joined_code.md
-cat markdown_source/06/*.md >> ./var/joined_code.md
-cat markdown_source/07/*.md >> ./var/joined_code.md
-cat markdown_source/08/*.md >> ./var/joined_code.md
-cat markdown_source/09/*.md >> ./var/joined_code.md
-cat markdown_source/10/*.md >> ./var/joined_code.md
-cat markdown_source/10/*.md >> ./var/joined_code.md
-cat markdown_source/11/*.md >> ./var/joined_code.md
-cat markdown_source/12/*.md >> ./var/joined_code.md
-cat markdown_source/13/*.md >> ./var/joined_code.md
-cat markdown_source/14/*.md >> ./var/joined_code.md
-cat markdown_source/15/*.md >> ./var/joined_code.md
+cat hugo/content/00/*.md > ./var/joined_code.md
+cat hugo/content/01/*.md >> ./var/joined_code.md
+cat hugo/content/02/*.md >> ./var/joined_code.md
+cat hugo/content/03/*.md >> ./var/joined_code.md
+cat hugo/content/04/*.md >> ./var/joined_code.md
+cat hugo/content/05/*.md >> ./var/joined_code.md
+cat hugo/content/06/*.md >> ./var/joined_code.md
+cat hugo/content/07/*.md >> ./var/joined_code.md
+cat hugo/content/08/*.md >> ./var/joined_code.md
+cat hugo/content/09/*.md >> ./var/joined_code.md
+cat hugo/content/10/*.md >> ./var/joined_code.md
+cat hugo/content/10/*.md >> ./var/joined_code.md
+cat hugo/content/11/*.md >> ./var/joined_code.md
+cat hugo/content/12/*.md >> ./var/joined_code.md
+cat hugo/content/13/*.md >> ./var/joined_code.md
+cat hugo/content/14/*.md >> ./var/joined_code.md
+cat hugo/content/15/*.md >> ./var/joined_code.md
 wc -l ./var/joined_code.md
 
 rm -rvf ./var/pics
